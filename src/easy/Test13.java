83e40d46b3e01cef6b4d77491b92627e28e3851b
张江浩 <zeno.jh.zhang@gmail.com>
2019-06-24 21:00:48 +0800

    Day1

---
+++ src/easy/Test13.java
@@ -0,0 +1,41 @@
+package easy;
+
+import java.util.*;
+
+/**
+ * 类的说明
+ *
+ * @author 张江浩
+ * @version 1.00
+ * @date 2019/6/24 20:18
+ */
+public class Test13 {
+    public int romanToInt(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        int before = 1000;
+        int total = 0;
+        char[] chars = s.toCharArray();
+        for (char c : chars
+        ) {
+            Integer now = map.get(c);
+            total += now;
+            if (before < now) {
+                total = total - 2 * before;
+            }
+            before = now;
+        }
+        return total;
+    }
+
+    public static void main(String[] args) {
+        Test13 test13 = new Test13();
+        System.out.println(test13.romanToInt("IV"));
+    }
+}
---
+++ src/easy/Test7.java
@@ -0,0 +1,38 @@
+package easy;
+
+/**
+ * 类的说明
+ *
+ * @author 张江浩
+ * @version 1.00
+ * @date 2019/6/22 12:28
+ */
+public class Test7 {
+    public int reverse(int x) {
+        if (x == 0 ){
+            return 0;
+        }
+        boolean flag = false;
+        String s = String.valueOf(x);
+        if (s.contains("-")){
+            flag = true;
+            s = s.replace("-","");
+        }
+        String reverse = new StringBuilder(s).reverse().toString();
+        Long l = Long.valueOf(reverse);
+        if (l > Integer.MAX_VALUE || l < Integer.MIN_VALUE){
+            return 0;
+        }
+        else {
+            if (flag){
+                reverse = "-"+reverse;
+            }
+            return Integer.valueOf(reverse);
+        }
+    }
+
+    public static void main(String[] args) {
+        Test7 test7 = new Test7();
+        System.out.println(test7.reverse(-22));
+    }
+}
