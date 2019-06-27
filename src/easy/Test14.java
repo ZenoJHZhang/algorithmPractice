02299daea58c56f927b76b37870aad91c0de89c9
张江浩 <zeno.jh.zhang@gmail.com>
2019-06-27 20:57:10 +0800

    DAY 4  最长公共前缀

---
+++ src/easy/Test14.java
@@ -0,0 +1,33 @@
+package easy;
+
+/**
+ * 最长公共前缀
+ *
+ * @author 张江浩
+ * @version 1.00
+ * @date 2019/6/27 20:41
+ */
+public class Test14 {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0){
+            return "";
+        }
+      String s = strs[0];
+        for (String str : strs) {
+            int j = 0;
+            for (; j < s.length() && j < str.length(); j++) {
+                if (str.charAt(j) != s.charAt(j)) {
+                    break;
+                }
+            }
+            s = s.substring(0, j);
+        }
+        return s;
+    }
+
+    public static void main(String[] args) {
+        Test14 test14 = new Test14();
+        String [] strs = new String[]{"ac","abc","acc"};
+        System.out.println(test14.longestCommonPrefix(strs));
+    }
+}
---
+++ src/easy/Test71.java
@@ -0,0 +1,11 @@
+package easy;
+
+/**
+ * 简化路径
+ *
+ * @author 张江浩
+ * @version 1.00
+ * @date 2019/6/27 20:56
+ */
+public class Test71 {
+}
