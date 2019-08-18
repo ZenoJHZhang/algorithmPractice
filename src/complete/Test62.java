73fd60006f4bb662c40e8300c91c89f14ac8f9bf
张江浩 <zeno.jh.zhang@gmail.com>
2019-08-18 10:32:13 +0800

    DAY26

--- src/complete/Test62.java
+++ src/complete/Test62.java
@@ -9,11 +9,25 @@ package complete;
  */
 public class Test62 {
     public int uniquePaths(int m, int n) {
+        if (m == 1 || n == 1) {
+            return 1;
+        }
+
         int[][] d = new int[m][n];
         d[0][1] = 1;
         d[1][0] = 1;
         for (int i = 1; i < m; i++) {
-
+            for (int j = 1; j < n; j++) {
+                d[0][j] = 1;
+                d[i][0] = 1;
+                d[i][j] = d[i - 1][j] + d[i][j - 1];
+            }
         }
+        return d[m - 1][n - 1];
+    }
+
+    public static void main(String[] args) {
+        Test62 test62 = new Test62();
+        System.out.println(test62.uniquePaths(7, 3));
     }
 }
