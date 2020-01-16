9885df3a2c5714ea231a5c6cb378dd1095b41e0a
张江浩 <zeno.jh.zhang@gmail.com>
2020-01-16 20:14:07 +0800

    DAY 41 完成

--- src/complete/Test47.java
+++ src/complete/Test47.java
@@ -16,6 +16,7 @@ public class Test47 {
      * 是否用过数组 used[] 标记元素是否用过
      * 一个数不能被重复使用
      * 一个数的前一个数如果被用过，且前一个等于它。剪枝
+     *
      * @param nums
      * @return
      */
@@ -23,28 +24,37 @@ public class Test47 {
         List<List<Integer>> res = new LinkedList<>();
         List<Integer> track = new LinkedList<>();
         Arrays.sort(nums);
-        back(nums,res,track);
+        int[] used = new int[nums.length];
+        back(nums, res, track, used);
         return res;
     }
 
-    private void back(int[] nums, List<List<Integer>> res, List<Integer> track) {
+    private void back(int[] nums, List<List<Integer>> res, List<Integer> track, int[] used) {
         if (track.size() == nums.length) {
             List<Integer> t = new LinkedList<>(track);
-            if (!res.contains(t)){
-                res.add(t);
-                return;
-            }
+            res.add(t);
+            return;
         }
-        for (int n : nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int n = nums[i];
+            int use = used[i];
+            if (use == 1) {
+                continue;
+            }
+            if (i > 0 && nums[i] == nums[i - 1] && used[i - 1] == 1) {
+                break;
+            }
             track.add(n);
-            back(nums, res, track);
+            used[i] = 1;
+            back(nums, res, track, used);
             track.remove(track.size() - 1);
+            used[i] = 0;
         }
     }
 
 
     public static void main(String[] args) {
-        int[] nums = {1,2,1};
+        int[] nums = {1, 2, 1};
         Test47 test47 = new Test47();
         List<List<Integer>> lists = test47.permuteUnique(nums);
         System.out.println(lists);
